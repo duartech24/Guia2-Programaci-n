782c6930c944f1dd1a555e54045a12ea555073a7
24CDL01-ChristianDuarte <160366029+duartech24@users.noreply.github.com>
2026-04-16 12:02:34 -0600

    Add files via upload

---
+++ Ejercicio3_Guia1.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("RANGO DE ESTUDIANTES");
+        Console.WriteLine("Ingrese una letra(A,B,C,D,F)");
+        char letra = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra)
+        {
+            case 'A': 
+                Console.WriteLine("Exelente");
+                break;
+            case 'B':
+                Console.WriteLine("Bueno");
+                break;
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+            case 'D':
+                Console.WriteLine("Deficiente");
+                break;
+            case 'F':
+                Console.WriteLine("Reprobado");
+                break;
+            default:
+                Console.WriteLine("Escriba una letra válida!!");
+                break ;
+        }
+    }
+}
